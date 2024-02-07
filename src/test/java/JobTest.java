4292ae50b13ff49475eef926683c6af1d2c34a77
Руслан Ситдиков <rnasitdikov@sberbank.ru>
2024-02-07 16:45:46 +0400

    NSIX-189:      feature: Оптимизирует импорты в тесте
    comment: Следующий этап - сраный XML

--- src/test/java/JobTest.java
+++ src/test/java/JobTest.java
@@ -2,16 +2,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import root.BatchApplication;
-import root.batch.job001.JobConfiguration;
 
-@SpringBatchTest
 @SpringBootTest(classes = BatchApplication.class)
-@ContextConfiguration(classes = { JobConfiguration.class })
 public class JobTest {
 
     @Autowired
